c41559ae0581f9acb125fd0836eb83374f5610b7
Jose Ramon Neira Vega <jramonnv0786@gmail.com>
2026-03-10 13:11:42 +0100

    Subo carpeta image/audio para Pull

--- src/mitrivial/view/VentanaPrincipal.java
+++ src/mitrivial/view/VentanaPrincipal.java
@@ -1,7 +1,18 @@
-import javax.swing.JFrame;
+import java.awt.Color;
 import javax.swing.*;
 
-public class VentanaPrincipal {
-    
+public class VentanaPrincipal extends JFrame{
 
+// Declaración de los botones para cada categoría.
+    private JButton Programacion;
+    private JButton LenguajeMarca;
+    private JButton BaseDatos;
+    private JButton Digitalizacion;
+
+    public VentanaPrincipal() {
+        setLayout(null); // Establecer un diseño absoluto para posicionar los botones manualmente.
+        setBackground(Color.LIGHT_GRAY); // Establecer el fondo del panel.
+
+
+    }
 }
