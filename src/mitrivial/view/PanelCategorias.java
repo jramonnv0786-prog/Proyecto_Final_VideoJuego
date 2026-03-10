4d1f6b13fc65ae50eb919fd938e41d6fdf25937d
juanma98117 <juanma98117@alumnos.ilerna.com>
2026-03-10 13:43:10 +0100

    Proyecto emigrado de eclipse a visual studio code

---
+++ src/mitrivial/view/PanelCategorias.java
@@ -0,0 +1,22 @@
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class PanelCategorias extends JPanel {
+
+	    private JButton historia;
+	    private JButton arte;
+	    private JButton deportes;
+	    private JButton ciencia;
+
+	    public PanelCategorias() {
+	        setLayout(null);
+	        setBackground(Color.blue);
+	        
+
+	                
+	    }
+	
+}
+
+
---
+++ src/mitrivial/view/PanelJuego.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import javax.swing.*;
+
+
+public class PanelJuego extends JPanel {
+
+	private JLabel Pregunta;
+    private JLabel Puntuacion;
+    private JButton[] respuestas;
+
+    public PanelJuego() {
+        setLayout(null);
+        setBackground(Color.blue);
+        
+
+                
+    }
+}
+
