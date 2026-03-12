c4d6498707fb0bc81d34ddbf4d5dad3cdccff489
Jose Ramon Neira Vega <jramonnv0786@gmail.com>
2026-03-12 12:54:41 +0100

    Botones de las asignaturas configurados

--- src/view/PanelCategorias.java
+++ src/view/PanelCategorias.java
@@ -30,8 +30,14 @@ public class PanelCategorias extends JPanel {
 		setBounds(0, 0, 1000, 800);
 
 		// BOTON PROGRAMACION
-		Programacion = new JButton("Programación");
+		ImageIcon iconProgramacion = new ImageIcon(getClass().getResource("/resources/Programacion.png.png"));
+		Image imgProgramacion = iconProgramacion.getImage().getScaledInstance(200, 80, Image.SCALE_SMOOTH);
+		Programacion = new JButton(new ImageIcon(imgProgramacion));
 		Programacion.setBounds(200, 150, 200, 80);
+		// Opcional: hacer que el botón se vea solo como la imagen
+		Programacion.setContentAreaFilled(false);
+		Programacion.setBorderPainted(false);
+		Programacion.setFocusPainted(false);
 		add(Programacion);
 
 		Programacion.addActionListener((ActionEvent e) -> {
@@ -47,8 +53,13 @@ public class PanelCategorias extends JPanel {
 		});
 
 		// BOTON ENTORNOS
-		EntornosDesarrollo = new JButton("Entornos de Desarrollo");
+		ImageIcon iconEntornos = new ImageIcon(getClass().getResource("/resources/Entornos.png.png"));
+		Image imgEntornos = iconEntornos.getImage().getScaledInstance(200, 80, Image.SCALE_SMOOTH);
+		EntornosDesarrollo = new JButton(new ImageIcon(imgEntornos));
 		EntornosDesarrollo.setBounds(600, 150, 200, 80);
+		EntornosDesarrollo.setContentAreaFilled(false);
+		EntornosDesarrollo.setBorderPainted(false);
+		EntornosDesarrollo.setFocusPainted(false);
 		add(EntornosDesarrollo);
 
 		EntornosDesarrollo.addActionListener((ActionEvent e) -> {
@@ -63,8 +74,13 @@ public class PanelCategorias extends JPanel {
 		});
 
 		// BOTON LENGUAJE
-		LenguajeMarca = new JButton("Lenguaje de Marcas");
+		ImageIcon iconLenguaje = new ImageIcon(getClass().getResource("/resources/Lenguajedemarca.png.png"));
+		Image imgLenguaje = iconLenguaje.getImage().getScaledInstance(200, 80, Image.SCALE_SMOOTH);
+		LenguajeMarca = new JButton(new ImageIcon(imgLenguaje));
 		LenguajeMarca.setBounds(200, 300, 200, 80);
+		LenguajeMarca.setContentAreaFilled(false);
+		LenguajeMarca.setBorderPainted(false);
+		LenguajeMarca.setFocusPainted(false);
 		add(LenguajeMarca);
 
 		LenguajeMarca.addActionListener((ActionEvent e) -> {
@@ -79,8 +95,13 @@ public class PanelCategorias extends JPanel {
 		});
 
 		// BOTON DIGITALIZACION
-		Digitalizacion = new JButton("Digitalización");
+		ImageIcon iconDigitalizacion = new ImageIcon(getClass().getResource("/resources/Digitalizacion.png.png"));
+		Image imgDigitalizacion = iconDigitalizacion.getImage().getScaledInstance(200, 80, Image.SCALE_SMOOTH);
+		Digitalizacion = new JButton(new ImageIcon(imgDigitalizacion));
 		Digitalizacion.setBounds(600, 300, 200, 80);
+		Digitalizacion.setContentAreaFilled(false);
+		Digitalizacion.setBorderPainted(false);
+		Digitalizacion.setFocusPainted(false);
 		add(Digitalizacion);
 
 		Digitalizacion.addActionListener((ActionEvent e) -> {
@@ -95,8 +116,13 @@ public class PanelCategorias extends JPanel {
 		});
 
 		// BOTON SISTEMA
-		SistemaInformatico = new JButton("Sistema Informatico");
+		ImageIcon iconSistema = new ImageIcon(getClass().getResource("/resources/Sistemas.png.png"));
+		Image imgSistema = iconSistema.getImage().getScaledInstance(200, 80, Image.SCALE_SMOOTH);
+		SistemaInformatico = new JButton(new ImageIcon(imgSistema));
 		SistemaInformatico.setBounds(200, 450, 200, 80);
+		SistemaInformatico.setContentAreaFilled(false);
+		SistemaInformatico.setBorderPainted(false);
+		SistemaInformatico.setFocusPainted(false);
 		add(SistemaInformatico);
 
 		SistemaInformatico.addActionListener((ActionEvent e) -> {
@@ -111,8 +137,13 @@ public class PanelCategorias extends JPanel {
 		});
 
 		// BOTON Sostenibilidad
-		Sostenibilidad = new JButton("Sostenibilidad");
+		ImageIcon iconSostenibilidad = new ImageIcon(getClass().getResource("/resources/Sostenibilidad.png.png"));
+		Image imgSostenibilidad = iconSostenibilidad.getImage().getScaledInstance(200, 80, Image.SCALE_SMOOTH);
+		Sostenibilidad = new JButton(new ImageIcon(imgSostenibilidad));
 		Sostenibilidad.setBounds(600, 450, 200, 80);
+		Sostenibilidad.setContentAreaFilled(false);
+		Sostenibilidad.setBorderPainted(false);
+		Sostenibilidad.setFocusPainted(false);
 		add(Sostenibilidad);
 
 		Sostenibilidad.addActionListener((ActionEvent e) -> {
