bd8bd94398e84620fbc1dcc443c5ec18218c1740
juanma98117 <juanma98117@alumnos.ilerna.com>
2026-03-13 09:23:42 +0100

    Main arreglado

--- src/main/Principal.java
+++ src/main/Principal.java
@@ -1,12 +1,32 @@
 package main;
 
 import view.Ventana;
+import view.PanelMenu;
+import view.PanelCategorias;
+import view.PanelEntornos;
+import view.PanelLenguajeMarca;
+import view.PanelDigitalizacion;
+import view.PanelSistemas;
+import view.PanelSostenibilidad;
+import view.PanelJuego;
+import view.PanelCreditos;
 
 public class Principal {
 
         public static void main(String[] args) {
 
+                model.Partida partida = new model.Partida("Jugador 1");
+
                 new Ventana();
+                new PanelMenu(partida);
+                new PanelCategorias(partida);
+                new PanelEntornos(partida);
+                new PanelLenguajeMarca(partida);
+                new PanelDigitalizacion(partida);
+                new PanelSistemas(partida);
+                new PanelSostenibilidad(partida);
+                new PanelJuego(partida, 0);
+                new PanelCreditos();
 
         }
 
